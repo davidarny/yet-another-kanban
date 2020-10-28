4a31bd70f8d70e2521d91548605d5873edf393a2
David Arutiunian <arutunjan666@mail.com>
2020-10-28 22:08:59 +0300

    ✅ Fix failing tests

--- client/src/app/sign-up/sign-up.component.spec.ts
+++ client/src/app/sign-up/sign-up.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from '../app-routing.module';
 
 import { SignUpComponent } from './sign-up.component';
 
@@ -8,9 +11,9 @@ describe('SignUpComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignUpComponent ]
-    })
-    .compileComponents();
+      declarations: [SignUpComponent],
+      imports: [ReactiveFormsModule, HttpClientModule, AppRoutingModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
